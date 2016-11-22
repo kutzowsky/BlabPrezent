2eaf3ccce29c8a86b27d4f45a897c55c51046a42
Michal K <kucyk010@gmail.com>
2016-11-22 20:19:34 +0100

    Writing usernames and addresses to sqlite database

--- MessageHandler.py
+++ MessageHandler.py
@@ -2,16 +2,25 @@
 # -*- coding: utf-8 -*-
 
 from MessageParser import MessageParser
+from DataManager import DataManager
 import Strings
 
-
+#todo: moduly zamiast klas ze statycznymi metodami
+#todo: testy na wykorzystanie DataManagera
 class MessageHandler(object):
     @staticmethod
+    #todo uporzadkowac
     def handle(message):
         if MessageParser.is_private(message):
             sender = MessageParser.get_sender_from(message)
             message_content = MessageParser.get_content_from(message)
             if message_content.startswith('BLABPREZENT'):
+                try:
+                    address = message_content.strip('BLABPREZENT ')
+                    DataManager.save_user_data(sender, address)
+                except:
+                    return MessageHandler.__create_private_message(sender, Strings.error_text)
+
                 return MessageHandler.__create_private_message(sender, Strings.data_saved)
             else:
                 return MessageHandler.__create_private_message(sender, Strings.help_text)
--- Strings.py
+++ Strings.py
@@ -3,3 +3,4 @@
 
 help_text = u'Jestem botem o bardzo małym rozumku i nie rozumiem :(. Pomoc na: http://...'
 data_saved = u'Zapisałem poprawnie Twoje dane. Cierpliwie czekaj na losowanie.'
+error_text = u'Oj, coś poszło nie tak. :('
