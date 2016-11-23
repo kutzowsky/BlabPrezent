726a8c03d71b7d63f80c08430e4ee04ca60c1301
Michal K <kucyk010@gmail.com>
2016-11-23 20:13:41 +0100

    Some cleanup in MessageHandler  * blabprezent command extracted to Strings

--- MessageHandler.py
+++ MessageHandler.py
@@ -8,28 +8,29 @@ import Strings
 #todo: moduly zamiast klas ze statycznymi metodami
 class MessageHandler(object):
     @staticmethod
-    #todo uporzadkowac
     def handle(message):
         sender = MessageParser.get_sender_from(message)
 
         if MessageParser.is_directed_private(message):
             message_content = MessageParser.get_content_from(message)
-            if message_content.startswith('BLABPREZENT'):
+
+            if message_content.startswith(Strings.blabprezent_command):
+                address = message_content.strip(Strings.blabprezent_command + ' ')
                 try:
-                    address = message_content.strip('BLABPREZENT ')
                     DataManager.save_user_data(sender, address)
                 except:
-                    return MessageHandler.__create_private_message(sender, Strings.error_text)
-
-                return MessageHandler.__create_private_message(sender, Strings.data_saved)
+                    answer = Strings.error_text
+                else:
+                    answer = Strings.data_saved
             else:
-                return MessageHandler.__create_private_message(sender, Strings.help_text)
+                answer = Strings.help_text
         else:
             if MessageParser.is_directed_public(message):
-
-                return MessageHandler.__create_private_message(sender, Strings.public_message_warn)
+                answer = Strings.public_message_warn
             else:
                 return None
+
+        return MessageHandler.__create_private_message(sender, answer)
 
     @staticmethod
     def __create_private_message(recipient, content):
