2e1f573142e69c82da64b508c67e128c8732e80a
Michal K <kucyk010@gmail.com>
2020-11-21 22:50:02 +0100

    Added bot that scans website to have a working project even if Blabler XMPP bot is dead  * for now it's just  an echo bot  * separate config entries for WWW login data - may differ from XMPP ones

--- config/BotConfiguration.py
+++ config/BotConfiguration.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-
-class BotConfiguration():
+class BotConfiguration:
     def __init__(self):
         self.jid = None
         self.password = None
         self.blabler_bot_jid = None
+
+        self.website_login = None
+        self.website_password = None
---
+++ wwwparsing/__init__.py
@@ -0,0 +1,1 @@
+from .BlabWebsiteClient import BlabWebsiteClient
